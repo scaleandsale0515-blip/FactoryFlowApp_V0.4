9040a0fb17b98f348d5ab0c26181f38ebc669224
scaleandsale0515-blip <scaleandsale0515@gmail.com>
2026-06-19 16:03:17 +0530

    Create app_colors.dart

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFFFF6B00);
+  static const danger = Color(0xFFE53935);
+  static const darkBorder = Color(0xFF2A2A2A);
+}
